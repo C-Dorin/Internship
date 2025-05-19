26b7c406787acc08da1bf82959529c3492fa72c2
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2025-05-19 15:17:29 +0300

    Configured the Project

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
+};
